3bc9572a5b8b026f07b69e6c927db47315ba8796
Dipesh Kandpal <kandpaldipesh@gmail.com>
2026-09-10 23:40:26 +0530

    Learn advanced js

---
+++ fun_with_js/fun.js
@@ -0,0 +1,13 @@
+const myArr = []
+%DebugPrint(myArr);
+
+//continous,Holey
+
+//SMI (small Integer) -> highest optimised
+// packed element
+// Double (flaot,string,function)
+
+// SMI > DOUBLE > PACKED
+// H_SMI > H_DOUBLE > H_PACKED
+
+// ek bar downgrade hua toh fir upgrade almost impossible
